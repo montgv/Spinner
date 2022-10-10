3c66419815d618672c3e0f1a26ced787795bc7fe
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-10-10 17:30:58 +0200

    Final commit

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import com.example.spinner.databinding.ActivityMainBinding;
@@ -16,6 +19,46 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, );
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.operacionesSpinner, R.layout.mi_spinner);
+        adapter.setDropDownViewResource(R.layout.mi_spinner);
+        binding.spinner.setAdapter(adapter);
+
+        binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String operacion = (String) adapterView.getItemAtPosition(i);
+                int valor1;
+                int valor2;
+
+                if (operacion.equals("Suma")) {
+                    valor1 = Integer.parseInt(binding.txtValor1.getText().toString());
+                    valor2 = Integer.parseInt(binding.txtValor2.getText().toString());
+                    binding.txtResultado.setText(String.valueOf(valor1 + valor2));
+                } else if (operacion.equals("Resta")) {
+                    valor1 = Integer.parseInt(binding.txtValor1.getText().toString());
+                    valor2 = Integer.parseInt(binding.txtValor2.getText().toString());
+                    binding.txtResultado.setText(String.valueOf(valor1 - valor2));
+                } else if (operacion.equals("Multiplicación")) {
+                    valor1 = Integer.parseInt(binding.txtValor1.getText().toString());
+                    valor2 = Integer.parseInt(binding.txtValor2.getText().toString());
+                    binding.txtResultado.setText(String.valueOf(valor1 * valor2));
+                } else if (operacion.equals("División")) {
+                    valor1 = Integer.parseInt(binding.txtValor1.getText().toString());
+                    valor2 = Integer.parseInt(binding.txtValor2.getText().toString());
+                    if (valor2 == 0) {
+                        binding.txtResultado.setText("No se puede dividir por 0");
+                    } else {
+                        binding.txtResultado.setText(String.valueOf(valor1 / valor2));
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
